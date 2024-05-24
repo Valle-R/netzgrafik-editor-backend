47bee7564d80e0c7baedf949d270f4070eab972e
Valentin Rouault <Valle-R@users.noreply.github.com>
2024-05-24 11:35:45 +0200

    Try changing adress

---
+++ src/main/java/ch/sbb/pfi/netzgrafikeditor/api/HealthController.java
@@ -0,0 +1,16 @@
+package ch.sbb.pfi.netzgrafikeditor.api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+public class HealthController {
+  @GetMapping("/v1/health")
+  public HttpStatus healthCheck() {
+    return HttpStatus.OK;
+  }
+}
